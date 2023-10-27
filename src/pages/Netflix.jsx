3b66913ccaa5eae713cca1007174a16994675679
krishna soni <soni01krishna@gmail.com>
2023-10-27 21:51:23 +0530

    working on api

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CardSlider from '../components/CardSlider'
+
 import HeroBanner from '../components/HeroBanner'
 import MainHeader from '../components/MainHeader'
 import { useNavigate } from 'react-router-dom'
